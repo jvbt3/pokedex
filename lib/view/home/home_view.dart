1f8df749a75c91c047d4d2fdc6b43bdae31039c7
jvbt3 <joaovitormartins077@gmail.com>
2023-04-13 16:52:10 -0300

    pokedex detail

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -4,8 +4,13 @@ import 'package:pokedex/view/detail/container/detail_container.dart';
 import '../../models/pokemon_model.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({Key? key, required this.list, required this.onSelected})
+   HomeView({Key? key, required this.list, required this.onSelected})
       : super(key: key);
+
+  final appBarBackground = const DecorationImage(
+    image: AssetImage('assets/images/pokemon.png'),
+    fit: BoxFit.cover,
+  );
 
   final List<Pokemon> list;
 
@@ -14,15 +19,45 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Pokédex',
+        style: TextStyle(
+          fontSize: 30,
+          fontFamily: 'Pokemon',
+        ),
+        ),
+        backgroundColor: Colors.red,
+      ),
       body: ListView.builder(
         itemCount: list.length,
         itemBuilder: (BuildContext ctx, int index) {
-          return ListTile(
-            title: Text(list[index].name),
-            onTap: () => onSelected(
-              '/pokemon-details',
-              DetailArguments(
-                name: list[index].name,
+          return SizedBox(
+            height: 200,
+            child: Card(
+              child: Column(
+                children: [
+                  Image.network(list[index].image,
+                  fit: BoxFit.contain,
+                  ),
+                  Material(
+                    elevation: 4,
+                    child: ListTile(
+                      title: Text(list[index].name,
+                      style: const TextStyle(
+                        letterSpacing: 1,
+                        fontSize: 20,
+                      ),
+                      ),
+                      onTap: () => onSelected(
+                        '/pokemon-details',
+                        DetailArguments(
+                          name: list[index].name,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -31,3 +66,5 @@ class HomeView extends StatelessWidget {
     );
   }
 }
+
+
